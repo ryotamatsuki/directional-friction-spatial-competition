fad55b956fa2130a86a457c7bffc9e979e09f65c
ryotamatsuki <60263857+ryotamatsuki@users.noreply.github.com>
2026-09-03 21:37:31 +0900

    Harden Stage 4R symbolic identity checks

--- verification/symbolic/stage4r_lsrs_verify.py
+++ verification/symbolic/stage4r_lsrs_verify.py
@@ -18,30 +18,30 @@ piR_out = sp.expand((1-x)*(pL-c+q_out))
 
 assert sp.simplify(sp.diff(piL_mid,x).subs(x,xstar)) == 0
 assert sp.simplify(sp.diff(piR_mid,x).subs(x,xstar)) == 0
-assert sp.factor(sp.diff(piL_mid,x,2)) == -2*(2*r+theta)
-assert sp.factor(sp.diff(piR_mid,x,2)) == -2*(2*r+theta)
-assert sp.factor(sp.diff(piL_out,x,2)) == -4*r
-assert sp.factor(sp.diff(piR_out,x,2)) == -4*r
+assert sp.simplify(sp.diff(piL_mid,x,2) + 2*(2*r+theta)) == 0
+assert sp.simplify(sp.diff(piR_mid,x,2) + 2*(2*r+theta)) == 0
+assert sp.simplify(sp.diff(piL_out,x,2) + 4*r) == 0
+assert sp.simplify(sp.diff(piR_out,x,2) + 4*r) == 0
 
-assert sp.factor(3*r*h-(C+h)) == 5*r**2+2*r*theta-2*r-theta
-assert sp.factor(sp.diff(piL_out,x).subs(x,r)) == -(10*r**2+r*theta-4*r-2*theta)/3
-assert sp.factor(sp.diff(piR_out,x).subs(x,r)-sp.diff(piR_mid,x).subs(x,r)) == theta*(r-1)
+assert sp.simplify((3*r*h-(C+h)) - (5*r**2+2*r*theta-2*r-theta)) == 0
+assert sp.simplify(sp.diff(piL_out,x).subs(x,r) + (10*r**2+r*theta-4*r-2*theta)/3) == 0
+assert sp.simplify((sp.diff(piR_out,x).subs(x,r)-sp.diff(piR_mid,x).subs(x,r)) - theta*(r-1)) == 0
 
-assert sp.factor(sp.diff(pL,r)) == (2*r+theta+2)/3
-assert sp.factor(sp.diff(pR,r)) == -(2*r+theta-4)/3
+assert sp.simplify(sp.diff(pL,r) - (2*r+theta+2)/3) == 0
+assert sp.simplify(sp.diff(pR,r) + (2*r+theta-4)/3) == 0
 
 upper = 2*r*(5*r-2)/(2-r)
 lower = 4-2*r
-assert sp.factor(upper-lower) == -4*(2*r**2+r-2)/(r-2)
+assert sp.simplify((upper-lower) + 4*(2*r**2+r-2)/(r-2)) == 0
 
 rv = sp.Rational(4,5)
 tv = sp.Rational(5,2)
-assert sp.simplify(xstar.subs({r:rv, theta:tv})) == sp.Rational(337,615)
-assert sp.simplify((pL-c).subs({r:rv, theta:tv})) == sp.Rational(337,150)
-assert sp.simplify((pR-c).subs({r:rv, theta:tv})) == sp.Rational(139,75)
-assert sp.simplify(sp.diff(pL,r).subs({r:rv, theta:tv})) == sp.Rational(61,30)
-assert sp.simplify(sp.diff(pR,r).subs({r:rv, theta:tv})) == -sp.Rational(1,30)
-assert sp.simplify(sp.diff(piL_out,x).subs({r:rv, theta:tv, x:rv, c:0})) == -sp.Rational(1,15)
+assert sp.simplify(xstar.subs({r:rv, theta:tv}) - sp.Rational(337,615)) == 0
+assert sp.simplify((pL-c).subs({r:rv, theta:tv}) - sp.Rational(337,150)) == 0
+assert sp.simplify((pR-c).subs({r:rv, theta:tv}) - sp.Rational(139,75)) == 0
+assert sp.simplify(sp.diff(pL,r).subs({r:rv, theta:tv}) - sp.Rational(61,30)) == 0
+assert sp.simplify(sp.diff(pR,r).subs({r:rv, theta:tv}) + sp.Rational(1,30)) == 0
+assert sp.simplify(sp.diff(piL_out,x).subs({r:rv, theta:tv, x:rv, c:0}) + sp.Rational(1,15)) == 0
 
 # theta=0 benchmark
 assert sp.simplify((pL-c).subs(theta,0) - r*(r+2)/3) == 0
